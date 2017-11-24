678b3c66e404562349e586c0c93e5eb2a2a71b44
43606 <alan_thatcher@navyfederal.org>
2017-11-24 08:28:19 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# multi-platform-ntp-multi-recipe
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: multi-platform-ntp-multi-recipe
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
