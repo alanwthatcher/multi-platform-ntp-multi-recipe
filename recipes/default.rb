3e0a459665553af61b21da23aba15a6e57bc1ea5
43606 <alan_thatcher@navyfederal.org>
2017-11-24 09:14:37 -0500

    base recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,11 +7,11 @@
 case node['platform']
 when 'redhat'
   # Find Red Hat release version
-  rv = node['platform_version'].split('.')[0]
+  pv = node['platform_version'].split('.')[0]
 
   # Call provider recipe based on platform version
-  include_recipe 'multi-platform-ntp-multi-recipe::redhat_ntpd' if rv == '6'
-  include_recipe 'multi-platform-ntp-multi-recipe::redhat_chrony' if rv == '7'
+  include_recipe 'multi-platform-ntp-multi-recipe::redhat_ntpd' if pv == '6'
+  include_recipe 'multi-platform-ntp-multi-recipe::redhat_chrony' if pv == '7'
 
 when 'aix'
   # Call AIX platform recipe
