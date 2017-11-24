9c6a803fdb9c1a9ca9f0d049085cb7bdf9eb7b44
43606 <alan_thatcher@navyfederal.org>
2017-11-24 09:13:13 -0500

    base recipes

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,18 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+case node['platform']
+when 'redhat'
+  # Find Red Hat release version
+  rv = node['platform_version'].split('.')[0]
+
+  # Call provider recipe based on platform version
+  include_recipe 'multi-platform-ntp-multi-recipe::redhat_ntpd' if rv == '6'
+  include_recipe 'multi-platform-ntp-multi-recipe::redhat_chrony' if rv == '7'
+
+when 'aix'
+  # Call AIX platform recipe
+  include_recipe 'multi-platform-ntp-multi-recipe::aix'
+
+end
